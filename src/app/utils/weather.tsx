764346d89ba84bb308eb1e720aaaf819854168dd
Luiz Fernando de Almeida Morais <luizfalmeidamorais@gmail.com>
2023-11-28 11:15:40 -0300

    Criação de projeto com Next.js

---
+++ src/app/utils/weather.tsx
@@ -0,0 +1,44 @@
+"use client"
+
+import { useEffect } from "react"
+
+const WeatherInfo = () => {
+    const [city, setCity] = useState("");
+
+    useEffect(() => {
+        const getWeather = async () => {
+        try {
+            const response = await fetch(
+            `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.NEXT_PUBLIC_OPENWEATHER_API_KEY}`
+            ).then((res) => res.json());
+            const weatherData = response.data;
+            setWeather({
+            description: weatherData.weather[0].description,
+            temperature: weatherData.main.temp,
+            });
+        } catch (err) {
+            console.log(err);
+        }
+        };
+    }, [])
+    return (
+        <div>
+        <form onSubmit={handleClick}>
+            <input
+            className="bg-slate-600 text-white"
+            type="text"
+            value={city}
+            onChange={handleChange}
+            />
+            <button type="submit">Procurar</button>
+        </form>
+    
+        {weather && (
+            <div>
+            <h1>Descrição: {weather.description}</h1>
+            <h1>Temperatura: {weather.temperature}</h1>
+            </div>
+        )}
+        </div>
+    )
+}
